c7400ab97b62beab16eb7627d324893abe915317
rachelyeshurun <rachelyeshurun@gmail.com>
2020-07-07 18:36:36 +0300

    quiz show question, allow retry + encouraging feedback phrases #5

--- notebooks/quizzes/quiz.py
+++ notebooks/quizzes/quiz.py
@@ -1,59 +1,72 @@
 import ipywidgets as widgets
 
 from IPython.display import display
-from ipywidgets import Button, Layout, Label
+from ipywidgets import Button, Layout, Label, AppLayout
 
 import json
 import sys
+import random
 
-def create_multi_answer_widget(options):
+first_try_flag = False
 
+def create_multi_answer_widget(question, options):
+
+    global first_try_flag
+    question_widget = Label(value=question)
     options_dict = {option['id']: widgets.Checkbox(description=option['answer'], value=False, layout=Layout(width='1000px'), style={'description_width': 'initial'}) for option in options}
     checkboxes = list(options_dict.values())
 
     # for each option, create a feedback box on the left and a checkbox on the right
     vertical = []
     for cb in checkboxes:
-        new_hbox = widgets.HBox([widgets.Label(value=''), cb])
+        new_hbox = widgets.HBox([Label(value=''), cb])
         vertical.append(new_hbox)
 
     # vertically laid out options with feedback on the left
     option_widget = widgets.VBox(vertical)
-    option_widget.box_style = 'info'
+    #option_widget.box_style = 'info'
 
     # submit button with (empty) score label on the left
     submit_button = Button(description='Submit')
     submit_button.style.button_color = 'lightblue'
-    score = Label(value='')
-    score_widget = widgets.HBox([score, submit_button])
+    score = Label(value = '', layout = Layout(width='500px'))
+    score_widget = widgets.HBox([submit_button, score])
     score_widget.box_style = 'info'
 
     # stack options and score area to make the final widget
-    multi_answer = widgets.VBox([option_widget, score_widget])
+
+    multi_answer = AppLayout(header=question_widget,
+              left_sidebar=option_widget,
+              center=None,
+              right_sidebar=None,
+              footer=score_widget)
+
+
+
+    #multi_answer = widgets.VBox([option_widget, score_widget])
     multi_answer.box_style = 'info'
 
     # question text will be in markdown before the quiz (for now) - easily allows interesting questions with code, formulas etc.
     # compare checkbox value with original
+    first_try_flag = True
 
     def check_answers(b):
 
         #run through each option, compare expected answer (checked/unchecked) with actual student answer
         num_options = len(options)
-        lost_points = 0
+        incorrect = 0
+        missing = 0
 
-        option_widget = multi_answer.children[0]
+        global first_try_flag
+        option_widget = multi_answer.children[2]
         score_widget = multi_answer.children[1]
 
-        # Remove the submit button
-        score_widget.children[1].layout.display = 'none'
-
-        score_label = score_widget.children[0]
-        score_label.layout = Layout(width='100px')
+        submit_button = score_widget.children[0]
+        score_label = score_widget.children[1]
 
         for i in range(num_options):
 
             opt = option_widget.children[i]
-
 
             lbl = opt.children[0]
             cb = opt.children[1]
@@ -62,15 +75,16 @@ def create_multi_answer_widget(options):
             expected_answer = options[i]['correct']
 
             # clear feedback before giving new feedback
-            #opt.box_style = 'info'
+            opt.layout = Layout(border=None)
+            cb.disabled = True
             lbl.value = ''
             lbl.layout=Layout(width='100px')
-            cb.disabled = True
+
 
             # feedback logic copied from Canvas quizzes
             # red border + 'incorrect' for incorrectly checked
             # green border + 'correct!' for correctly checked
-            # grey border + text 'correct answer' for incorrectly unchecked
+            # NOT DOING THIS: grey border + text 'correct answer' for incorrectly unchecked
             # nothing for correctly unchecked - try to even disable
             if (expected_answer and actual_answer):
                 #opt.box_style = 'success'
@@ -78,30 +92,54 @@ def create_multi_answer_widget(options):
                 lbl.value = 'Correct!'
             if (expected_answer and not actual_answer):
                 #opt.box_style = 'warning'
-                lbl.value = 'Correct answer'
-                opt.layout = Layout(border='3px solid gray')
-                lost_points += 1
+                #lbl.value = 'Correct answer'
+                #opt.layout = Layout(border='3px solid gray')
+                missing += 1
             if (not expected_answer and actual_answer):
                 #opt.box_style = 'danger'
                 lbl.value = 'Incorrect'
                 opt.layout = Layout(border='2px solid red')
-                lost_points += 1
+                incorrect += 1
 
         # update the score label
-        text = 'Score:  ' + str(num_options - lost_points) + '/' + str(num_options) + ' pts'
-        if lost_points == 0:
-            score_label.value = text + ' ;-)'
+        if incorrect + missing == 0:
+
             #$\ddot\smile$'
             option_widget.box_style = 'success'
             score_widget.box_style = 'success'
             multi_answer.box_style = 'success'
+            submit_button.disabled = True
+            submit_button.style.button_color = 'lightgreen'
+            cb.disabled = True
+
+            if first_try_flag == True:
+                text = random.choice(['Perfect!', 'Your\'re on a roll!', 'Keep up the good work!', 'All correct!', 'Correct!',
+                                  'Success!', '100% !!', 'Perfect ;-) ', 'Excellent!', 'You got it!', 'You\'re the best',
+                                             'You got an A!', 'Nice job, rock star!', 'Hurray for you!'])
+            else:
+                text = 'Very good'
         else:
             option_widget.box_style = 'danger'
             score_widget.box_style = 'danger'
             multi_answer.box_style = 'danger'
-            score_label.value = text
+            submit_button.style.button_color = 'pink'
+
+            first_try_flag = False
+            submit_button.description = 'Submit again'
+            for i in range(num_options):
+                opt = option_widget.children[i]
+                cb = opt.children[1]
+                cb.disabled = False
+            # clear rectangles
+            if missing > 0:
+                text = 'you missed some correct options, try again!'
+            else:
+                text = 'try again!'
+
+        score_label.value = '~ ~ ~ ~ ~' + text + '~ ~ ~ ~ ~'
 
     submit_button.on_click(check_answers)
+
     return(multi_answer)
 
 
@@ -111,7 +149,7 @@ def main(argv):
         quiz_dict = json.load(f)
 
     # assuming for now, just one type of quiz: multi-answer multiple choice
-    w = create_multi_answer_widget(quiz_dict['options'])
+    w = create_multi_answer_widget(quiz_dict['question'], quiz_dict['options'])
     display(w)
 
 
