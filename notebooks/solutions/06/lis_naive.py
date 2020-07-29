2fddccc5bfa20eb10ba32edf5b397f7491e71f02
rachelyeshurun <rachelyeshurun@gmail.com>
2020-07-29 22:07:41 +0300

    #18 added naive solution

--- notebooks/solutions/06/lis_naive.py
+++ notebooks/solutions/06/lis_naive.py
@@ -1,27 +1,51 @@
-# Solution from here https://www.techiedelight.com/longest-increasing-subsequence-using-dynamic-programming/
-# but I started from the end of the sequence, just as a matter of preference.
-def lis(top, v):
+# solution based on this code: https://www.geeksforgeeks.org/longest-increasing-subsequence-dp-3/  by NIKHIL KUMAR SINGH 
+
+# maximum over all n
+global maximum 
+  
+def max_ending_here(v): 
+  
+    global maximum 
+  
     n = len(v)
+    
+    # base cases
     if n == 0:
         return 0
-
-    # exclude the last item
-    excluded = lis(v[n - 1], v[0:n -1])
-    
-    # include the last item - but only if it's less than the current item at top of sequence
-    if v[n - 1] < top:
-        included =  1 + lis(v[n - 1], v[0:n -1])
-    else:
-        included = 0
-
-    return max(included, excluded)
-
-
-def solve(v):
-    # start with infinite, so the last item in the list gets a chance to be in the final subsequence
-    return lis(float('inf'), v)
-          
+    if n == 1: 
+        return 1
+  
+    # max_length_ending_here is the length of the longest increasing subsequence ending with v[n-1] 
+    max_length_ending_here = 1
+  
+    for i in range(1, n): 
+        # recursively get all LIS ending with v[0], ..., v[i - 2]
+        res = max_ending_here(v[0: i]) 
         
-# unit tests
-# YOUR CASES HERE ###
-assert(solve(v = [3, 1, 4, 1, 5, 1, 9, 2, 6]) == 5)
+        # if an element before v[n-1] is smaller than v[n-1] and its LIS is greater, then its LIS becomes the new local maximum
+        if v[i - 1] < v[n-1] and res + 1 > max_length_ending_here: 
+            max_length_ending_here = res + 1
+  
+    # update overall maximum 
+    maximum = max(maximum , max_length_ending_here) 
+  
+    return max_length_ending_here 
+  
+def lis(v): 
+  
+    global maximum 
+  
+    # will be updated by the recursive function 'max_ending_here' 
+    maximum = 0
+  
+    m = max_ending_here(v)
+    print ('maximum ending here: ', m, 'global maximum: ', maximum)
+    
+    return maximum
+  
+#unit tests
+assert(lis([3, 1, 4, 1, 5, 1, 9, 2, 6]) == 4)
+assert(lis([3, 1, 4, 1, 5, 1, 9, 2, 6, 5]) == 4)
+assert(lis([1, 2, 3]) == 3)
+assert(lis([3, 2, 1, 0]) == 1)
+assert(lis([]) == 0)
